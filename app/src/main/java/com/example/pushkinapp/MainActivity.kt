bf96ecadf7b4fe55515809ffb5c9229fa4022570
deniko <rdeniko@gmail.com>
2021-11-09 16:21:30 +0700

    New commit

--- app/src/main/java/com/example/pushkinapp/MainActivity.kt
+++ app/src/main/java/com/example/pushkinapp/MainActivity.kt
@@ -8,73 +8,56 @@ import android.widget.TextView
 import com.example.pushkinapp.R
 import kotlin.random.Random
 
-private const val MY_OWN_LOG_TAG = "MyOwnLog"
-private const val VALUE = "Value"
+private const val TAG = "MyLog"
+
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var pushkinButton: Button
-    private lateinit var pushkinLines: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        pushkinLines = findViewById(R.id.my_text)
-        pushkinButton = findViewById(R.id.my_button)
 
-        if(savedInstanceState != null)
-            pushkinLines.text = savedInstanceState.getString(VALUE)
-
-
-        pushkinButton.setOnClickListener { pushkin() }
-        Log.d(MY_OWN_LOG_TAG, "OnCreate()")
-    }
-
-    private fun pushkin(){
-        val verse: List<String> = listOf("Ты видел деву на скале", "В одежде белой над волнами", "Когда, бушуя в бурной мгле,", "Играло море с берегами,", "Когда луч молний озарял", "Ее всечасно блеском алым", "И ветер бился и летал", "С ее летучим покрывалом?", "Прекрасно море в бурной мгле", "И небо в блесках без лазури;", "Но верь мне: дева на скале", "Прекрасней волн, небес и бури.")
-        for(i in verse){
-            println(i).toString().also { pushkinLines.text = it }
-        }
+        Log.i(TAG,"Ты видел деву на скале")
     }
 
     override fun onStart() {
         super.onStart()
 
-        Log.d(MY_OWN_LOG_TAG, "OnStart()")
+        Log.i(TAG, "В одежде белой над волнами")
+        Log.i(TAG, "Когда, бушуя в бурной мгле,")
     }
 
     override fun onResume() {
         super.onResume()
 
-        Log.d(MY_OWN_LOG_TAG, "onResume()")
+        Log.i(TAG, "Играло море с берегами,")
+        Log.i(TAG, "Когда луч молний озарял")
     }
 
     override fun onPause() {
         super.onPause()
 
-        Log.d(MY_OWN_LOG_TAG, "onPause()")
+        Log.i(TAG, "Ее всечасно блеском алым")
+        Log.i(TAG, "И ветер бился и летал")
     }
 
     override fun onStop() {
         super.onStop()
 
-        Log.d(MY_OWN_LOG_TAG, "onStop()")
+        Log.i(TAG, "С ее летучим покрывалом?")
+        Log.i(TAG, "Прекрасно море в бурной мгле")
     }
 
     override fun onRestart() {
         super.onRestart()
 
-        Log.d(MY_OWN_LOG_TAG, "onRestart()")
+        Log.i(TAG, "И небо в блесках без лазури;")
+        Log.i(TAG, "Но верь мне: дева на скале")
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        Log.d(MY_OWN_LOG_TAG, "onDestroy()")
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(VALUE, pushkinLines.text.toString())
+        Log.i(TAG, "Прекрасней волн, небес и бури.")
     }
 }
